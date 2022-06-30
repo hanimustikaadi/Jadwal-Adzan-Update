55ec879d438523635a3ab5ebecbb297cd81c4c3e
hani <mustikahani@yahoo.com>
2022-06-30 21:48:13 +0700

    update

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
-content = requests.get('https://kalam.sindonews.com/jadwalsholat')
 
+def ekstraksi_data():
 
-soup = BeautifulSoup(content.text, 'html.parser')
-title = (soup.find('title')).text
-print(title)
+    try:
+        content= requests.get('https://kalam.sindonews.com/jadwalsholat')
+    except Exception:
+        return None
+    if content.status_code == 200:
 
+       soup = BeautifulSoup(content.text, 'html.parser')
+       title = (soup.find('title')).text
+       result= soup.find('tr',{'class':'current-date'})
+       result= result.findChildren('td')
 
-result= soup.find('tr',{'class':'current-date'})
-result= result.findChildren('td')
+       hasil = dict()
+       hasil['Tanggal'] = result[0].text
+       hasil['Subuh'] = result[1].text
+       hasil['imsak'] = result[2].text
+       hasil['dhuhur'] = result[3].text
+       hasil['ashar'] = result[4].text
+       hasil['magrib'] = result[5].text
+       hasil['isya'] = result[6].text
+       return hasil
+    else:
+        return None
 
+def tampilkan_data(result):
 
-i=0
-for res in result:
-    print(i , res)
-    i=i+1
+    if result is None:
+        print('Tidak bisa menemukan jadwal Adzan Solat')
+        return
 
+    print('Update Adzan Untuk DKI Jakarta Sekitarnya')
+    print(f'Tanggal Hari Ini { result["Tanggal"]}')
+    print(f'Waktu imsak { result["imsak"]}')
+    print(f'Waktu Adzan Subuh { result["Subuh"]}')
+    print(f'Waktu Adzan dhuhur { result["dhuhur"]}')
+    print(f'Waktu Adzan ashar { result["ashar"]}')
+    print(f'Waktu Adzan magrib { result["magrib"]}')
+    print(f'Waktu Adzan isya { result["isya"]}')
 
-hasil = dict()
-hasil['Tanggal'] = result[0].text
-hasil['Subuh'] = result[1].text
-hasil['imsak'] = result[2].text
-hasil['dhuhur'] = result[3].text
-hasil['ashar'] = result[4].text
-hasil['magrib'] = result[5].text
-hasil['isya'] = result[6].text
+if __name__ == '__main__':
 
-print('Update Adzan Untuk DKI Jakarta Sekitarnya')
-print(f'Tanggal Hari Ini { hasil["Tanggal"]}')
-print(f'Waktu imsak { hasil["imsak"]}')
-print(f'Waktu Adzan Subuh { hasil["Subuh"]}')
-print(f'Waktu Adzan dhuhur { hasil["dhuhur"]}')
-print(f'Waktu Adzan ashar { hasil["ashar"]}')
-print(f'Waktu Adzan magrib { hasil["magrib"]}')
-print(f'Waktu Adzan isya { hasil["isya"]}')
+    result = ekstraksi_data()
+    tampilkan_data(result)
 
 
 
